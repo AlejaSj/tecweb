cf1bd322e61f50eba078d3408a1d12608a213a02
Alejandro <alejandrosj015@gmail.com>
2024-09-10 14:09:36 -0600

    Practica 7 Ejercicio 2

--- practicas/p07/src/funciones.php
+++ practicas/p07/src/funciones.php
@@ -1,14 +1,24 @@
 <?php
-    if(isset($_GET['numero']))
-    {
-        $num = $_GET['numero'];
-        if ($num%5==0 && $num%7==0)
+    function numero(){
+        if(isset($_GET['numero']))
         {
-            echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            $num = $_GET['numero'];
+            if ($num%5==0 && $num%7==0)
+            {
+                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            }
+            else
+            {
+                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            }
         }
-        else
+    }
+    function nombre(){
+    if(isset($_POST["name"]) && isset($_POST["email"]))
         {
-            echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            echo $_POST["name"];
+            echo '<br>';
+            echo $_POST["email"];
         }
     }
 ?>
